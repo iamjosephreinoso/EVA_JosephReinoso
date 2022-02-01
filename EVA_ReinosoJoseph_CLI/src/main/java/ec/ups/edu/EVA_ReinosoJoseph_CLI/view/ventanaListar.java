b6e98b9455b3d6635d71fee253595a33c7462fc9
iamjosephreinoso <69018206+iamjosephreinoso@users.noreply.github.com>
2022-02-01 13:32:05 -0500

    Commit 2
    Evaluacion Terminada, Informe Completo

--- EVA_ReinosoJoseph_CLI/src/main/java/ec/ups/edu/EVA_ReinosoJoseph_CLI/view/ventanaListar.java
+++ EVA_ReinosoJoseph_CLI/src/main/java/ec/ups/edu/EVA_ReinosoJoseph_CLI/view/ventanaListar.java
@@ -5,22 +5,48 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
+import javax.xml.namespace.QName;
+
+import ec.ups.edu.EVA_ReinosoJoseph_CLI.soap.ObrasServiceSOAP;
+import ec.ups.edu.EVA_ReinosoJoseph_CLI.soap.ObrasServiceSOAPService;
+
 import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.awt.event.ActionEvent;
 
 public class ventanaListar extends JFrame {
 
 	private JPanel contentPane;
-	private JTextPane txtLista;
+	private JTextArea txtLista;
+	private static final QName SERVICE_NAME = new QName("http://bean.EVA_ReinosoJoseph_SRV.ups.edu.ec/", "ObrasServiceSOAPService");
+	private static ObrasServiceSOAP port;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		URL wsdlURL = ObrasServiceSOAPService.WSDL_LOCATION;
+        if (args.length > 0 && args[0] != null && !"".equals(args[0])) {
+            File wsdlFile = new File(args[0]);
+            try {
+                if (wsdlFile.exists()) {
+                    wsdlURL = wsdlFile.toURI().toURL();
+                } else {
+                    wsdlURL = new URL(args[0]);
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        ObrasServiceSOAPService ss = new ObrasServiceSOAPService(wsdlURL, SERVICE_NAME);
+        port = ss.getObrasServiceSOAPPort();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -48,16 +74,24 @@ public class ventanaListar extends JFrame {
 		lblNewLabel.setBounds(161, 22, 168, 13);
 		contentPane.add(lblNewLabel);
 		
-		txtLista = new JTextPane();
+		txtLista = new JTextArea();
 		txtLista.setBounds(10, 46, 416, 180);
 		contentPane.add(txtLista);
 		
 		JButton btnListar = new JButton("Listar");
 		btnListar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				listar();
 			}
 		});
 		btnListar.setBounds(180, 236, 85, 21);
 		contentPane.add(btnListar);
+	}
+	public void listar(){
+		 java.util.List<ec.ups.edu.EVA_ReinosoJoseph_CLI.soap.ObrasArte> _getObras__return = port.getObras();
+	        System.out.println("getObras.result=" + _getObras__return);
+	        for(int i = 0; i<_getObras__return.size();i++) {
+	        	txtLista.append(_getObras__return.toString()+"\n");
+	        }
 	}
 }
--- EVA_ReinosoJoseph_SRV/src/main/java/ec/edu/ups/EVA_ReinosoJoseph_SRV/model/ObrasArte.java
+++ EVA_ReinosoJoseph_SRV/src/main/java/ec/edu/ups/EVA_ReinosoJoseph_SRV/model/ObrasArte.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,6 +16,7 @@ public class ObrasArte implements Serializable{
 		
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "obr_id")
 	private String id;
 	@Column(name = "obr_nombre")
